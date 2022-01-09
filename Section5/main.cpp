4543e3e1da0e93159572cfb15986a8850eb229cf
Jie Wu <jie.wu@MARK-C02TF2SZG8WN.local>
2022-01-09 20:08:47 +0800

    upload the code for section 5 challenge

---
+++ Section5/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	/*
+	 * start of the main program
+	 * seems the same to python
+	 */
+	printf("hello world\n");
+	return 0; // should return integer
+}
